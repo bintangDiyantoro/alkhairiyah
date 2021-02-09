31c62fa5f875177c3cf34c15952222e7c9fd0731
Bean <bintangdiyantoro@gmail.com>
2021-02-09 13:07:38 +0700

    Login bug fixed

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -41,7 +41,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="<?= base_url('admin/dashboard') ?>">
+                <a class="nav-link" href="<?= base_url('admin') ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
