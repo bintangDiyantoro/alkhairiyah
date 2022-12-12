21716eee7cca87519bb3b14e8353647162d785e1
Bean <bintangdiyantoro@gmail.com>
2022-12-12 14:01:54 +0700

    PWA Coinditional Fallback

--- assets/js/app3.js
+++ assets/js/app3.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw2.js')
+    navigator.serviceWorker.register('/sw3.js')
         .then(() => true)
         .catch(err => console.log('service worker not registered', err));
 }
