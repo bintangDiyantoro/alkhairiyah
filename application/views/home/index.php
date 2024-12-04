613a70a2d2f6a491745f309a0d8d427e22416f46
Bean <bintangdiyantoro@gmail.com>
2024-12-04 22:48:54 +0700

    fixed issues on students data inserts from excel

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,121 +1,72 @@
 <div class="jbtr lazyload">
     <div class="container">
         <h2 class="display-3">Ahlan Wa Sahlan!</h2>
-        <p class="lead">Selamat datang di laman SD Islam Al-Khairiyah Banyuwangi &#128591;</p>
     </div>
 </div>
 <div class="container-fluid">
-    <?php if (count($berita) > 100) : ?>
-        <div class="card card-2 card-3" style="margin-top: -70px">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-9">
-                        <h3 class="display-4 text-center">Berita Terkini</h3>
-                        <div id="carouselExampleCaptions" class="carousel slide mt-3" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <?php
-                                if (count($berita) <= 4) :
-                                    for ($i = 0; $i < count($berita); $i++) : ?>
-                                        <li data-target="#carouselExampleCaptions" data-slide-to="<?= $i ?>" class="c-ind"></li>
-                                    <?php endfor;
-                                else :
-                                    for ($i = 0; $i <= 3; $i++) : ?>
-                                        <li data-target="#carouselExampleCaptions" data-slide-to="<?= $i ?>" class="c-ind"></li>
-                                <?php endfor;
-                                endif; ?>
-                            </ol>
-                            <div class="carousel-inner">
-                                <?php if (count($berita) <= 4) :
-                                    for ($j = 0; $j <= count($berita) - 1; $j++) : ?>
-                                        <div class="carousel-item">
-                                            <a href="berita/detail/<?= $berita[$j]['id'] - 1 ?>">
-                                                <?php if ($berita[$j]['image']) : ?>
-                                                    <img src="<?= base_url() . $berita[$j]['image'] ?>" class="d-block rounded w-100" alt="...">
-                                                <?php elseif ($berita[$j]['yt']) : ?>
-                                                    <lite-youtube videoid="<?= $berita[$j]['yt'] ?>">
-                                                        <!-- <iframe class="iframe" src="https://www.youtube.com/embed/<?= $berita[$j]['yt'] ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loading="lazy" allowfullscreen></iframe> -->
-                                                    </lite-youtube>
-                                                <?php endif; ?>
-                                                <div class="carousel-caption d-none d-md-block">
-                                                    <h2 class="news-heading"><?= $berita[$j]['title'] ?></h2>
-                                                    <p class="news-heading"><?= $berita[$j]['prev'] ?>...</p>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    <?php endfor;
-                                else :
-                                    for ($k = 0; $k <= 3; $k++) : ?>
-                                        <div class="carousel-item">
-                                            <a href="berita/detail/<?= $berita[$k]['id'] - 1 ?>">
-                                                <?php if ($berita[$k]['image']) : ?>
-                                                    <img src="<?= base_url() . $berita[$k]['image'] ?>" class="d-block rounded w-100" alt="...">
-                                                <?php elseif ($berita[$k]['yt']) : ?>
-                                                    <lite-youtube videoid="<?= $berita[$k]['yt'] ?>">
-                                                        <!-- <iframe class="iframe" src="https://www.youtube.com/embed/<?= $berita[$k]['yt'] ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-                                                    </lite-youtube>
-                                                <?php endif; ?>
-                                                <div class="carousel-caption d-none d-md-block">
-                                                    <h2 class="news-heading"><?= $berita[$k]['title'] ?></h2>
-                                                    <p class="news-heading"><?= $berita[$k]['prev'] ?>...</p>
-                                                </div>
-                                            </a>
-                                        </div>
-                                <?php endfor;
-                                endif; ?>
-                            </div>
-                            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
+    <div class="card card-2 card-3 mt-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9">
+                    <h2 class="text-center mb-3 main-section">Selamat Datang di <strong>SD Islam Al-Khairiyah Banyuwangi</strong></h2>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="./assets/img/78964067382af8722d2f18b5d211faa90fef5c8e_s2_n2_y2(1).webp" alt="Foto Kepala Sekolah SD Islam Al-Khairiyah Banyuwangi" class="img-thumbnail mb-3">
                         </div>
-                        <!-- <p class="lead text-center">Selamat datang di halaman utama SDI Al Khairiyah </p>
-                    <hr class="my-4"> -->
-                        <!-- <a class="btn btn-primary btn" href="<?= base_url('pendaftaran') ?>" role="button">Daftarkan Siswa Baru</a> -->
+                        <div class="col-md-8">
+                            <p style="text-indent: 50px;">
+                                <strong>SD Islam Al-Khairiyah Banyuwangi</strong> adalah lembaga pendidikan yang berkomitmen mencetak generasi unggul dengan mengintegrasikan ilmu pengetahuan, teknologi, dan nilai-nilai Islami dalam setiap aspek pembelajaran. Berlokasi di lingkungan yang asri dan kondusif, kami menghadirkan suasana belajar yang nyaman untuk mendukung perkembangan akademik, spiritual, dan karakter siswa.
+                            </p>
+                            <p style="text-indent: 50px;">
+                                Dengan didukung oleh tenaga pendidik profesional, fasilitas modern, dan program unggulan berbasis akhlak mulia, SD Islam Al-Khairiyah Banyuwangi mengedepankan pendidikan holistik yang menginspirasi siswa untuk berprestasi, kreatif, dan memiliki jiwa kepemimpinan. Selain itu, kami juga menanamkan nilai-nilai Al-Qur’an dan hadits dalam keseharian, sehingga siswa tidak hanya cerdas secara intelektual, tetapi juga memiliki integritas moral yang kuat.
+                            </p>
+                        </div>
                     </div>
-                    <div class="col-lg-3">
-                        <h3 class="text-center my-3"><strong>Semua Berita</strong></h3>
-                        <?php if (count($berita) <= 15) : ?>
-                            <ul>
-                                <?php for ($i = 0; $i <= count($berita) - 1; $i++) : ?>
-                                    <li>
-                                        <a href="berita/detail/<?= $berita[$i]['id'] - 1 ?>">
-                                            <?php
-                                            $str = $berita[$i]['content'];
-                                            $length = strlen(substr($str, strpos($str, "<p>") + 3));
-                                            $link = strip_tags(substr($str, 0, -$length));
-                                            echo $link;
-                                            ?>
-                                        </a>
-                                        &nbsp;<small>(<?= $berita[$i]['date'] ?>)</small>
-                                    </li>
-                                <?php endfor; ?>
-                            </ul>
-                        <?php else : ?>
-                            <ul>
-                                <?php for ($i = 0; $i <= 14; $i++) : ?>
-                                    <li>
-                                        <a href="berita/detail/<?= $berita[$i]['id'] - 1 ?>">
-                                            <?php
-                                            $str = $berita[$i]['content'];
-                                            $length = strlen(substr($str, strpos($str, "<p>") + 3));
-                                            $link = strip_tags(substr($str, 0, -$length));
-                                            echo $link;
-                                            ?>
-                                        </a>
-                                        &nbsp;<small>(<?= $berita[$i]['date'] ?>)</small>
-                                    </li>
-                                <?php endfor; ?>
-                            </ul>
-                        <?php endif; ?>
-                    </div>
+                    <p style="text-indent: 50px;">
+                        Bergabunglah bersama kami dan jadilah bagian dari keluarga besar SD Islam Al-Khairiyah Banyuwangi, tempat di mana masa depan gemilang dimulai dengan langkah kecil yang penuh makna. Kami percaya, bersama-sama, kita dapat membangun generasi penerus yang siap menghadapi tantangan dunia dengan iman, ilmu, dan amal.
+                    </p>
                 </div>
+                <div class="col-lg-3 right-bar">
+                    <h3 class="text-center my-3"><strong>Berita</strong></h3>
+                    <?php if (count($berita) <= 5) : ?>
+                        <ul>
+                            <?php for ($i = 0; $i <= count($berita) - 1; $i++) : ?>
+                                <li>
+                                    <a href="berita/detail/<?= $berita[$i]['id'] - 1 ?>">
+                                        <?php
+                                        $str = $berita[$i]['content'];
+                                        $length = strlen(substr($str, strpos($str, "<p>") + 3));
+                                        $link = strip_tags(substr($str, 0, -$length));
+                                        echo $link;
+                                        ?>
+                                    </a>
+                                    &nbsp;<small>(<?= $berita[$i]['date'] ?>)</small>
+                                </li>
+                            <?php endfor; ?>
+                        </ul>
+                    <?php else : ?>
+                        <ul>
+                            <?php for ($i = 0; $i <= 6; $i++) : ?>
+                                <li>
+                                    <a href="berita/detail/<?= $berita[$i]['id'] - 1 ?>">
+                                        <?php
+                                        $str = $berita[$i]['content'];
+                                        $length = strlen(substr($str, strpos($str, "<p>") + 3));
+                                        $link = strip_tags(substr($str, 0, -$length));
+                                        echo $link;
+                                        ?>
+                                    </a>
+                                    &nbsp;<small>(<?= $berita[$i]['date'] ?>)</small>
+                                </li>
+                            <?php endfor; ?>
+                        </ul>
+                    <?php endif; ?>
+                </div>
+
             </div>
         </div>
-    <?php endif; ?>
+    </div>
+
     <div class="card card-1 card-3 mt-3">
         <div class="container">
             <div class="row">
@@ -210,77 +161,3 @@
         </div>
     </div>
 </div>
-
-
-<!-- Modal -->
-<!-- <div class="modal fade" id="MyModal" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title font-weight-bolder" id="MyModalTitle">Selamat! Data anda sudah tersimpan</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-5">
-                        ID Pendaftaran</br>
-                    </div>
-                    <div class="col-md-5 font-weight-bolder">
-                        <?= $calon_siswa['id'] ?> <br>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-5">
-                        Nama</br>
-                    </div>
-                    <div class="col-md-5 font-weight-bolder">
-                        <?= $calon_siswa['nama'] ?> <br>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-5">
-                        Jenis kelamin</br>
-                    </div>
-                    <div class="col-md-5 font-weight-bolder">
-                        <?= $calon_siswa['jenis_kelamin'] ?> <br>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-5">
-                        Tanggal Lahir</br>
-                    </div>
-                    <div class="col-md-5 font-weight-bolder">
-                        <?= $calon_siswa['tgl_lahir'] ?> <br>
-                    </div>
-                </div>
-                <?php if ($calon_siswa['asal_tk'] !== null || $calon_siswa['asal_tk'] !== "") : ?>
-                    <div class="row">
-                        <div class="col-md-5">
-                            TK Asal</br>
-                        </div>
-                        <div class="col-md-5 font-weight-bolder">
-                            <?= $calon_siswa['asal_tk'] ?> <br>
-                        </div>
-                    </div>
-                <?php endif; ?>
-                <div class="row">
-                    <div class="col-md-5">
-                        Wali</br>
-                    </div>
-                    <div class="col-md-5 font-weight-bolder">
-                        <?= $calon_siswa['namawali'] ?> <br>
-                    </div>
-                </div>
-                </br>Klik tombol <strong>Cetak PDF</strong> untuk menyimpan data dalam format PDF sebagai bukti pendaftaran.
-                </br>
-                <p>Pengumuman siswa diterima dapat dilihat pada tanggal <strong>13 April 2020</strong>, untuk melihat pengumuman klik menu <strong> Pendaftaran > Lihat data calon siswa</strong></p>
-
-            </div>
-            <div class="modal-footer">
-                <a class="btn btn-info" href="<?= base_url('pendaftaran/cetak/') . $calon_siswa['id_asli'] ?>" target="_blank" rel="noopener noreferrer">Bukti Pendaftaran</a><button type="button" class="btn btn-primary" id="modal-close" data-dismiss="modal">Ok</button>
-            </div>
-        </div>
-    </div>
-</div> -->
